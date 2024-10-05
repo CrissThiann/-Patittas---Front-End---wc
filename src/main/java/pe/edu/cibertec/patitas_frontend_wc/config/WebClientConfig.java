770e374d231daea44ee83d1c27e3a86355463dc0
Cristhian Fernando Vasquez Albornoz <cristhianalbornoz9@gmail.com>
2024-10-04 23:26:31 -0500

    Primer commit clase 04/10/24

---
+++ src/main/java/pe/edu/cibertec/patitas_frontend_wc/config/WebClientConfig.java
@@ -0,0 +1,31 @@
+package pe.edu.cibertec.patitas_frontend_wc.config;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebClientConfig {
+
+    HttpClient httpClient = HttpClient.create()
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000) // timeout de conexion
+            .responseTimeout(Duration.ofSeconds(10)) //timeout para obetener el total de la respuesta
+            .doOnConnected(conn -> conn.addHandlerLast(new ReadTimeoutHandler(10, TimeUnit.SECONDS)));//timeout para la recepcion de cada paquete
+
+
+    @Bean
+    public WebClient webClientAuthentication(WebClient.Builder builder) {
+        return builder
+                .baseUrl("http://localhost:8090/autenticacion")
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
+}
---
+++ src/main/java/pe/edu/cibertec/patitas_frontend_wc/controller/LoginController.java
@@ -0,0 +1,105 @@
+package pe.edu.cibertec.patitas_frontend_wc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import pe.edu.cibertec.patitas_frontend_wc.dto.LoginRequestDTO;
+import pe.edu.cibertec.patitas_frontend_wc.dto.LoginResponseDTO;
+import pe.edu.cibertec.patitas_frontend_wc.viewmodel.LoginModel;
+import reactor.core.publisher.Mono;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @Autowired
+    WebClient webClientAuthentication;
+
+
+    @GetMapping("/inicio")
+    public String inicio(Model model){
+        LoginModel loginModel = new LoginModel("00","","");
+        model.addAttribute("loginModel",loginModel);
+        return "inicio";
+
+    }
+
+    @PostMapping("/autenticar")
+    public String autenticar(@RequestParam("tipoDocumento") String tipoDocumento,
+                             @RequestParam("numeroDocumento") String numeroDocumento,
+                             @RequestParam("password") String password,
+                             Model model) {
+
+        // Validar campos de entrada
+        if (tipoDocumento == null || tipoDocumento.trim().length() == 0 ||
+                numeroDocumento == null || numeroDocumento.trim().length() == 0 ||
+                password == null || password.trim().length() == 0) {
+            LoginModel loginModel = new LoginModel("01", "Error: Debe completar correctamente sus credenciales", "");
+            model.addAttribute("loginModel", loginModel);
+            return "inicio";
+
+        }
+
+        try {
+
+            // Invocar servicio de autenticación
+            //String endpoint = "http://localhost:8090/autenticacion/login";
+            LoginRequestDTO loginRequestDTO = new LoginRequestDTO(tipoDocumento, numeroDocumento, password);
+            //LoginResponseDTO loginResponseDTO = webClientAuthentication.postForObject("/login", loginRequestDTO, LoginResponseDTO.class);
+
+            Mono<LoginResponseDTO> monoLoginResponseDTO = webClientAuthentication.post()
+                    .uri("http://localhost:8090/autenticacion/login")
+                    .body(Mono.just(loginRequestDTO), LoginRequestDTO.class)
+                    .retrieve()
+                    .bodyToMono(LoginResponseDTO.class);
+
+            //recuperar resultado moodo bloquenate (Sincronico)
+            LoginResponseDTO loginResponseDTO = monoLoginResponseDTO.block();
+
+            if (loginResponseDTO.codigo().equals("00")){
+
+                LoginModel loginModel = new LoginModel("00", "", loginResponseDTO.nombreUsuario());
+                model.addAttribute("loginModel", loginModel);
+                return "principal";
+
+            } else {
+
+                LoginModel loginModel = new LoginModel("02", "Error: Autenticación fallida", "");
+                model.addAttribute("loginModel", loginModel);
+                return "inicio";
+
+            }
+
+        } catch(Exception e) {
+
+            LoginModel loginModel = new LoginModel("99", "Error: Ocurrió un problema en la autenticación", "");
+            model.addAttribute("loginModel", loginModel);
+            System.out.println(e.getMessage());
+            return "inicio";
+
+        }
+
+
+//        //Llamada al servicio de auntenticacion del backend
+//        String backendurl = "http://localhost:8090/autenticacion/login";
+//        LoginRq loginRq = new LoginRq(tipoDocumento, numeroDocumento, password);
+//        LoginModel loginModel = restTemplate.postForObject(backendurl, loginRq, LoginModel.class);
+//
+//        if (loginModel != null && "00".equals(loginModel.codigo())) {
+//            model.addAttribute("loginModel", loginModel);
+//            return "principal";
+//        }
+//        model.addAttribute("loginModel", new LoginModel("01", "Credenciales erroneas..", ""));
+//        return "inicio";
+
+
+
+
+    }
+}
